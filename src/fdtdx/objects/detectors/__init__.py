1221c50883651fccd8e06198e7c3ef28ebb3ef10
ymahlau <mahlauyannik@gmail.com>
2025-03-04 14:44:01 +0100

    better detector import

--- src/fdtdx/objects/detectors/__init__.py
+++ src/fdtdx/objects/detectors/__init__.py
@@ -12,3 +12,15 @@ Key constraints include:
 The constraints are implemented as modules that can be chained together
 and applied during the optimization process.
 """
+
+from .diffractive import DiffractiveDetector
+from .energy import EnergyDetector
+from .phasor import PhasorDetector
+from .poynting_flux import PoyntingFluxDetector
+
+__all__ = [
+    "DiffractiveDetector",
+    "EnergyDetector",
+    "PhasorDetector",
+    "PoyntingFluxDetector",
+]
