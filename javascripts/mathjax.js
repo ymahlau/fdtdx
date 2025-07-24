ab07d315f2106b3884262074cb88c6802f0c2622
ymahlau <mahlauyannik@gmail.com>
2025-07-24 11:56:45 +0200

    Deployed e4ad121 with MkDocs version: 1.6.1

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -1,19 +1,7 @@
-window.MathJax = {
+MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
+    inlineMath: [['$', '$'], ["\\(", "\\)"]],
     processEscapes: true,
-    processEnvironments: true
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
   }
-};
+}
 
-document$.subscribe(() => { 
-  MathJax.startup.output.clearCache()
-  MathJax.typesetClear()
-  MathJax.texReset()
-  MathJax.typesetPromise()
-})
