c3e576d607aa653c23dc6e89891efde13650debf
Yannik Mahlau <59509701+ymahlau@users.noreply.github.com>
2026-05-12 19:53:14 +0200

    Revert "feat: GDS/GDSII import for layer stacks, port sources, and port detec…"
    This reverts commit 84ce0733617c6a01ef1ddff0e4b2462e9ac9c78f.

--- src/fdtdx/__init__.py
+++ src/fdtdx/__init__.py
@@ -102,15 +102,6 @@ from fdtdx.objects.sources.profile import (
     TemporalProfile,
 )
 from fdtdx.objects.static_material.cylinder import Cylinder
-from fdtdx.objects.static_material.gds_layer_stack import (
-    GDSLayerObject,
-    GDSLayerSpec,
-    GDSPortSpec,
-    detectors_from_gds_ports,
-    gds_layer_stack,
-    gds_layer_stack_from_component,
-    sources_from_gds_ports,
-)
 from fdtdx.objects.static_material.polygon import (
     ExtrudedPolygon,
     extruded_polygon_from_gds,
@@ -149,9 +140,6 @@ __all__ = [
     "ExtrudedPolygon",
     "FieldDetector",
     "FieldState",
-    "GDSLayerObject",
-    "GDSLayerSpec",
-    "GDSPortSpec",
     "GaussianPlaneSource",
     "GaussianPulseProfile",
     "GaussianSmoothing2D",
@@ -216,7 +204,6 @@ __all__ = [
     "compute_mode",
     "compute_poynting_flux",
     "constants",
-    "detectors_from_gds_ports",
     "export_arrays_snapshot_to_vti",
     "export_json",
     "export_json_str",
@@ -230,8 +217,6 @@ __all__ = [
     "frozen_field",
     "frozen_private_field",
     "full_backward",
-    "gds_layer_stack",
-    "gds_layer_stack_from_component",
     "import_from_json",
     "metric_efficiency",
     "normalize_by_energy",
@@ -247,6 +232,5 @@ __all__ = [
     "resolve_object_constraints",
     "run_fdtd",
     "setup_sparams_simulation",
-    "sources_from_gds_ports",
     "wavelength_to_period",
 ]
