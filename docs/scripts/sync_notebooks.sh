3869b2ee1a46ed8eb7829045e41250927b6a19f4
renaissancenerd <vatsallimbachiya@gmail.com>
2026-09-15 17:09:44 +0200

    removed generate cards

--- docs/scripts/sync_notebooks.sh
+++ docs/scripts/sync_notebooks.sh
@@ -9,13 +9,6 @@ mkdir -p docs/source/notebooks/physics
 mkdir -p docs/source/notebooks/components
 mkdir -p docs/source/notebooks/advanced
 
-# Remove notebooks that no longer exist upstream, so deleted/renamed
-# notebooks don't leave stale pages or stale cards behind
-rm -f docs/source/notebooks/quickstart/*.ipynb
-rm -f docs/source/notebooks/physics/*.ipynb
-rm -f docs/source/notebooks/components/*.ipynb
-rm -f docs/source/notebooks/advanced/*.ipynb
-
 mv fdtdx-notebooks-main/quickstart/*.ipynb docs/source/notebooks/quickstart
 mv fdtdx-notebooks-main/physics/*.ipynb docs/source/notebooks/physics
 mv fdtdx-notebooks-main/components/*.ipynb docs/source/notebooks/components
@@ -24,6 +17,3 @@ mv fdtdx-notebooks-main/advanced/*.ipynb docs/source/notebooks/advanced
 # Clean up
 rm repo.zip
 rm -rf fdtdx-notebooks-main
-
-# Regenerate the notebook gallery cards on each landing page
-python3 docs/source/generate_cards.py
